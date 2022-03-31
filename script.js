596c35e86a358e7dadc7462e9a20d357d0655434
Tomás Caraballo (Yomi) <yomizana@gmail.com>
2022-03-31 13:44:50 -0400

    Figure out how to get width and height of the container

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const grid = document.createElement('div');
 
 grid.classList.add('grid');
 gridContainer.appendChild(grid);
+
+let containerHeight = gridContainer.offsetHeight;
+let containerWidth = gridContainer.offsetWidth;
+
+console.log (containerHeight, containerWidth);
